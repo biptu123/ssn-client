1d73ca14fbd402dc26aa3d42d9f8b2bad1c83ffc
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-01-15 11:13:01 +0530

    Reapply "Basic Frontend and Server Initialization"
    This reverts commit 94fe9b715248e4f34b1323a1d927ac5e413d1d0b.

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <>
+      <Layout>
+        <section className="hero-section">
+          <h1>About</h1>
+          <p>About</p>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Products = () => {
+  return (
+    <>
+      <Layout>
+        <section className="hero-section">
+          <h1>Products</h1>
+          <p>Products</p>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default Products;
