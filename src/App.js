94fe9b715248e4f34b1323a1d927ac5e413d1d0b
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-01-15 11:09:04 +0530

    Revert "Basic Frontend and Server Initialization"
    This reverts commit 7f5241747e22ce1439320585b9c99273476d4f1a.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import Products from "./pages/Products";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Home from "./pages/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
